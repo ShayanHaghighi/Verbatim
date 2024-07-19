9fca0672763b5d48946787eb67296b29ebe8638d
Shayan Haghighi <sxh1498@student.bham.ac.uk>
2024-07-20 00:44:55 +0100

    added service folder

---
+++ Frontend/src/service/deck-helper.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import IDeck from "../models/deck-model";
+
+// this function should request all of the decks for the current logged in user. (can be done with GET request to /deck)
+// the function should then parse the response and create a list of IDeck objects, and then return those IDeck objects
+// pro tip 😎: try making the right API requests in Postman, then translate the request you did to this function
+function get_all_decks(access_token: string): IDeck[] {
+  return [];
+}
+
+// arguments: deck_name, access_token
+// send a request to the backend to create a new deck entry
+// return the results (status code/message)
+function create_deck() {}
+
+function delete_deck() {}
+
+function update_deck() {}
+
+// feel free to add helper functions
